f544694dac55fcabce222557e8370b419740e85a
Pritam-Sing-2025 <pritamsing2025@gmail.com>
2026-02-08 11:30:42 +0530

    states switching using buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import './App.css'
 
 import Accept from './components/accept.jsx';
@@ -6,20 +6,57 @@ import Argue from './components/argue.jsx';
 import Reject from './components/reject.jsx';
 import Explain from './components/explain.jsx';
 
-
 function App() {
+  const [uiState, setUiState] = useState("reject"); //accept, argue, reject, explain
+  const renderUI = () => {
+    switch (uiState) {
+      case "accept":
+        return <Accept />
+      case "argue":
+        return <Argue />
+      case "reject":
+        return <Reject />
+      case "explain":
+        return <Explain />
+    }
+  };
   return (
-    <div style={{padding: "40px", fontFamily: "sans-serif"}}>
-      {/* <h1>Accept Demo</h1>
-      <Accept /> */}
-      {/* <h1>Argue Demo</h1>
-      <Argue /> */}
-      <h1>Reject Demo</h1>
-      <Reject /> 
-      {/* <h1>Explanation Demo</h1>
-      <Explain />  */}
-    </div>
-  )
+        <div style={{padding: "40px", fontFamily: "sans-serif"}}>
+          <h1>ArgueUI Demo</h1>
+          <div style={{marginBottom: "20px"}}>
+            <button onClick={() => setUiState("accept")}>Accept</button>
+            <button onClick={() => setUiState("argue")}>Argue</button>
+            <button onClick={() => setUiState("reject")}>RejectExplain</button>
+            <button onClick={() => setUiState("explain")}>Explain</button>
+          </div>
+          {renderUI()}
+
+
+          {/* <h1>Accept Demo</h1>
+          <Accept /> */}
+          {/* <h1>Argue Demo</h1>
+          <Argue /> */}
+          {/* <h1>Reject Demo</h1>
+          <Reject />  */}
+          {/* <h1>Explanation Demo</h1>
+          <Explain />  */}
+        </div>
+      )
 }
+
+// function App() {
+//   return (
+//     <div style={{padding: "40px", fontFamily: "sans-serif"}}>
+//       {/* <h1>Accept Demo</h1>
+//       <Accept /> */}
+//       {/* <h1>Argue Demo</h1>
+//       <Argue /> */}
+//       <h1>Reject Demo</h1>
+//       <Reject /> 
+//       {/* <h1>Explanation Demo</h1>
+//       <Explain />  */}
+//     </div>
+//   )
+// }
 
 export default App;
